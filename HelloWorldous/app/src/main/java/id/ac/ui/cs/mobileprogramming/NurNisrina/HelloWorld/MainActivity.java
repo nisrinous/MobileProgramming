e9c3448c7ba7906f848c7af1d2ece658fa8c5187
Nisrinous <nisrinous@gmail.com>
2020-09-28 23:28:58 +0700

    Modifikasi fitur

--- HelloWorldous/app/src/main/java/id/ac/ui/cs/mobileprogramming/NurNisrina/HelloWorld/MainActivity.java
+++ HelloWorldous/app/src/main/java/id/ac/ui/cs/mobileprogramming/NurNisrina/HelloWorld/MainActivity.java
@@ -3,6 +3,9 @@ package id.ac.ui.cs.mobileprogramming.NurNisrina.HelloWorld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton greetingImage = (ImageButton)findViewById(R.id.greetingImage);
+        greetingImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = StaticFunctions.GreetingMessage("Hello!");
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
